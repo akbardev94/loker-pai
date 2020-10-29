a95614925f658f62e6ed45ddee75b63a6ee876f3
Anas Maulana Akbar <48192604+akbardev94@users.noreply.github.com>
2020-10-29 16:09:32 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // konfigurasi database
 $host       =   "localhost";
-$user       =   "root";
-$password   =   "";
-$database   =   "loker-pai";
+$user       =   "id15260635_lokerpai";
+$password   =   "oQU95gV0Xc_tI=IF";
+$database   =   "id15260635_loker";
 // perintah php untuk akses ke database
 $koneksi = mysqli_connect($host, $user, $password, $database);
 ?>
